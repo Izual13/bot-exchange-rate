adddeb2102dc25d5a30f150a40bcbcd4dac89d9d
Maxim Tsoy <mtsoy1990@gmail.com>
2018-01-23 00:52:45 +0300

    refactoring and added scheduled task for listening chat

--- src/clj_tools/core.clj
+++ src/clj_tools/core.clj
@@ -2,25 +2,23 @@
   (:require [org.httpkit.client :as http]
             [clojure.tools.nrepl.server :as nrepl.server]
             [clojure.data.json :as json]
-            ; [clojure.core.async :as async :refer :all]
             [cider.nrepl :as cider]))
 
 
 
-(def post-message-url (str "https://api.telegram.org/bot" token "/" "sendMessage"))
 (def bitcoin-url "https://api.coinmarketcap.com/v1/ticker/bitcoin/")
 (def last-message-id (atom 1))
 (def chat-ids (atom #{}))
 (def is-updating (atom false))
 (def token (get (System/getenv) "TOKEN" "token..."))
+
+(def post-message-url (str "https://api.telegram.org/bot" token "/" "sendMessage"))
 (def get-updates-url (str "https://api.telegram.org/bot" token "/" "getUpdates"))
 (defn parse[str] (get (first (json/read-str str :key-fn keyword)) :price_usd))
-(defn get-updates-request [] (json/write-str {"offset" (+ @last-message-id 1) "timeout" 10}))
+(defn get-updates-request [] (json/write-str {"offset" (+ @last-message-id 1) "timeout" 60}))
 (defn post-message-request [chat-id text] (json/write-str {"chat_id" chat-id
   "text" text
   "parse_mode" "Markdown"}))
-
-;(swap! chat-ids conj 6)
 
 (defn add-chat-id [id] (
   if (not (contains? @chat-ids id))
@@ -31,30 +29,21 @@
   if (contains? @chat-ids id)
     (swap! chat-ids disj id)))
 
-
 (defn update-last-message-id [id] (reset! last-message-id id))
 
 
 (defn get-updates []  
-  (if (not @is-updating) (do 
-    (reset! is-updating true)
+  (if (not @is-updating) 
+    (do (reset! is-updating true)
     (let [{:keys [status headers body error] :as resp} 
-      @(http/post get-updates-url {:body (get-updates-request)
-                                   :headers {"Content-Type" "application/json"}})]
-    (if error 
-      (println "Failed, exception: " error)    
-      (println "HTTP GET success: " body))
-    (reset! is-updating false)
-    (println status)
-    body))))
+        @(http/post get-updates-url {:body (get-updates-request)
+                                     :headers {"Content-Type" "application/json"}})]
+        (if error 
+          (println "Failed, exception: " error)    
+          (println "HTTP GET success: " body))
+        (reset! is-updating false)
+        body))))
   
-(get-updates)
-(println @is-updating)
-(reset! is-updating false)
-(main-task)
-(println @is-updating)
-
-
 (defn get-usd []  
   (let [{:keys [status headers body error] :as resp} @(http/get bitcoin-url)]
     (if error 
@@ -63,50 +52,34 @@
 
 (defn post-message [chat-id]
   (let [{:keys [status headers body error] :as resp} 
-    @(http/post post-message-url {:body (post-message-request chat-id (str "Bitcoin price: " (get-usd)))
+    @(http/post post-message-url {:body (post-message-request chat-id (str "*Bitcoin price*: _" (get-usd) "_"))
                                   :headers {"Content-Type" "application/json"}})]
   (if error 
     (println "Failed, exception: " error)    
-    (println "HTTP GET success: " body))
+    (println "HTTP GET success: " status))
     body))
 
+    
+(defn parse-updates[updates] 
+  (if (not (nil? updates)) (:result (json/read-str updates :key-fn keyword))))
 
-      
-    (get-usd)
+(defn change-action [text chat-id update-id] 
+  (case text
+    "/start" (add-chat-id chat-id)
+    "/stop" (remove-chat-id chat-id)
+    "/getExchange" (post-message chat-id)
+    (println "Message: " text " chatId: " chat-id " updateId:" update-id)))
 
-    ; (clojure.set/union #{1 2} #{3 4})
-    (get-updates)
-    
-    
-   
-    
-    
-    (defn parse-updates[updates] 
-      (if (not (nil? updates)) (json/read-str updates :key-fn keyword)))
-    
-    
-    (defn change-action [text chat-id update-id] 
-      (case text
-        "/start" (add-chat-id chat-id)
-        "/stop" (remove-chat-id chat-id)
-        "/getExchange" (post-message chat-id)
-        (println text)
-    ))
+        
+(defn message-handler [{update-id :update_id {{chat-id :id} :chat text :text} :message}] 
+  (do (println text) (change-action text chat-id update-id) (update-last-message-id update-id)))        
 
-(defn main-task [] 
-  (->> (parse-updates (get-updates)) :result 
-  (map (fn [{update-id :update_id {{chat-id :id} :chat text :text} :message}] 
-    (do (change-action text chat-id update-id) (update-last-message-id update-id))))))    
-  
-    
-(nil? (get-updates))     
-(main-task)
+(defn main-task [] (doseq [message (parse-updates (get-updates))] (message-handler message)))
+
 
 (defn set-interval [callback ms] 
-  (future (while true (do (Thread/sleep ms) (callback)))))
-      
-(def job (set-interval #(main-task) 1000))
+  (future (while true (do (callback) (Thread/sleep ms)))))
 
-(main-task)
-
-(future-cancel job)
+(defn -main [] (def job (set-interval #(main-task) 1000)))
+  
+  
