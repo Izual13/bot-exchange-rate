82d500f0f83c36849c21a90fa49334a3bfd0b49b
Maxim Tsoy <mtsoy1990@gmail.com>
2018-01-24 22:38:09 +0300

    updated leiningen and fixed test

--- project.clj
+++ project.clj
@@ -2,9 +2,13 @@
   :description "clj-tools"
   :dependencies [
     [org.clojure/clojure "1.9.0"]
-    [http-kit "2.2.0"]
-  ])
+    [cider/cider-nrepl "0.16.0"]
+    [org.clojure/data.json "0.2.6"]
+    [org.clojure/tools.namespace "0.2.11"]
+    [http-kit "2.2.0"]]
+  :main clj-tools.core
+  :aot [clj-tools.core])
 
 
-  
+
   
--- test/clj_tools/core_test.clj
+++ test/clj_tools/core_test.clj
@@ -12,5 +12,5 @@
 
 (deftest parse-updates-test
   (testing "parse updates"
-    (is (=  (parse-updates json-updates) {:ok true, :result [{:update_id 12, :message {:message_id 1234, :chat {:id 142657573, :first_name "m", :last_name "q", :username "mq", :type "private"}, :date 154394, :text "/getExchange"}} {:update_id 123, :message {:message_id 123, :chat {:id 987, :first_name "m", :last_name "q", :username "mq", :type "private"}, :date 154394, :text "ю"}}]}))))
+    (is (=  (parse-updates json-updates) [{:update_id 12, :message {:message_id 1234, :chat {:id 142657573, :first_name "m", :last_name "q", :username "mq", :type "private"}, :date 154394, :text "/getExchange"}} {:update_id 123, :message {:message_id 123, :chat {:id 987, :first_name "m", :last_name "q", :username "mq", :type "private"}, :date 154394, :text "ю"}}]))))
     
